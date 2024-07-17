819c2910fdf168a217d9ade15f1ed29abccce627
Sudibya <sudibya24@gmail.com>
2024-07-17 18:32:42 +0530

    new code

--- ServerLibrary/Data/AppDbContext.cs
+++ ServerLibrary/Data/AppDbContext.cs
@@ -8,6 +8,13 @@ namespace ServerLibrary.Data
     public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<GeneralDepartment> GeneralDepartment { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Town> Town { get; set; }
+
+        public DbSet<Users> Users { get; set; }
+
      
     }
 }
